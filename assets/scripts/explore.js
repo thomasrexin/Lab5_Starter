7c2b68326632e0cbdf343d0f0c437fd9ff585c77
Thomas Rexin <tjrexin@ucsd.edu>
2022-10-30 18:43:58 -0700

    add modified expose.js and explore.js files

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,3 +5,57 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+
+const synth = window.speechSynthesis;
+
+const voiceSelect = document.querySelector('select');
+const text = document.querySelector('textarea');
+const button = document.querySelector('button');
+const img = document.querySelector('img');
+
+let voices = [];
+let inputText = "";
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+text.addEventListener('input', function handleChange(event) {
+  inputText = event.target.value;
+})
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const utterThis = new SpeechSynthesisUtterance(inputText);
+  const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  synth.speak(utterThis);
+  utterThis.onstart = function() {
+    img.src = `assets/images/smiling-open.png`;
+  }
+  utterThis.onend = function() {
+    img.src = `assets/images/smiling.png`;
+  }
+})
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,3 +5,43 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+
+const hornSelect = document.querySelector("[name='horn']");
+const img = document.querySelector("[alt='No image selected']");
+const audio = document.querySelector('.hidden');
+
+hornSelect.addEventListener('change', (event) => {
+  img.src = `assets/images/${event.target.value}.svg`;
+  audio.src = `assets/audio/${event.target.value}.mp3`
+})
+
+const volumeSelect = document.querySelector("[name='volume']");
+const volumeImg = document.querySelector("[alt='Volume level 2']");
+
+volumeSelect.addEventListener('input', updateVolume);
+
+function updateVolume(num) {
+  if (num.target.value == 0) {
+    volumeImg.src = `assets/icons/volume-level-0.svg`;
+  }
+  else if (num.target.value < 33) {
+    volumeImg.src = `assets/icons/volume-level-1.svg`;
+  }
+  else if (num.target.value < 67) {
+    volumeImg.src = `assets/icons/volume-level-2.svg`;
+  }
+  else {
+    volumeImg.src = `assets/icons/volume-level-3.svg`;
+  }
+  audio.volume = num.target.value / 100;
+}
+
+const confetti = new JSConfetti();
+const playSound = document.querySelector('button');
+
+playSound.addEventListener('click', () => {
+  audio.play();
+  if (img.currentSrc.endsWith("party-horn.svg")) {
+    confetti.addConfetti();
+  }
+})
